f926b777a65dc751b8820721c01a9f4320071b53
Mark Hamstra <hello@markhamstra.com>
2021-10-01 18:35:17 +0200

    Reformat like it's 2021

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -5,10 +5,11 @@
  * @return string The file's content
  * @author splittingred
  */
-function getSnippetContent($filename = '') {
+function getSnippetContent($filename = '')
+{
     $o = file_get_contents($filename);
-    $o = str_replace('<?php','',$o);
-    $o = str_replace('?>','',$o);
+    $o = str_replace('<?php', '', $o);
+    $o = str_replace('?>', '', $o);
     $o = trim($o);
     return $o;
 }
@@ -20,130 +21,144 @@ $tstart = $mtime;
 set_time_limit(0);
 
 /* define package */
-define('PKG_NAME','CookieList');
-define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.0.0');
-define('PKG_RELEASE','pl');
+define('PKG_NAME', 'CookieList');
+define('PKG_NAME_LOWER', strtolower(PKG_NAME));
+define('PKG_VERSION', '1.0.0');
+define('PKG_RELEASE', 'pl');
 
-$root = dirname(dirname(__FILE__)).'/';
-$sources= array (
+$root = dirname(dirname(__FILE__)) . '/';
+$sources = [
     'root' => $root,
-    'build' => $root .'_build/',
+    'build' => $root . '_build/',
     'resolvers' => $root . '_build/resolvers/',
     'data' => $root . '_build/data/',
-    'source_core' => $root.'core/components/'.PKG_NAME_LOWER,
-    'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
-    'plugins' => $root.'core/components/'.PKG_NAME_LOWER.'/elements/plugins/',
-    'snippets' => $root.'core/components/'.PKG_NAME_LOWER.'/elements/snippets/',
-    'lexicon' => $root . 'core/components/'.PKG_NAME_LOWER.'/lexicon/',
-    'docs' => $root.'core/components/'.PKG_NAME_LOWER.'/docs/',
-    'model' => $root.'core/components/'.PKG_NAME_LOWER.'/model/',
-);
+    'source_core' => $root . 'core/components/' . PKG_NAME_LOWER,
+    'source_assets' => $root . 'assets/components/' . PKG_NAME_LOWER,
+    'plugins' => $root . 'core/components/' . PKG_NAME_LOWER . '/elements/plugins/',
+    'snippets' => $root . 'core/components/' . PKG_NAME_LOWER . '/elements/snippets/',
+    'lexicon' => $root . 'core/components/' . PKG_NAME_LOWER . '/lexicon/',
+    'docs' => $root . 'core/components/' . PKG_NAME_LOWER . '/docs/',
+    'model' => $root . 'core/components/' . PKG_NAME_LOWER . '/model/',
+];
 unset($root);
 
 require_once $sources['root'] . 'config.core.php';
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 
-$modx= new modX();
+$modx = new modX();
 $modx->initialize('mgr');
 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->setLogTarget('ECHO'); echo 'Packing '.PKG_NAME_LOWER.'-'.PKG_VERSION.'-'.PKG_RELEASE.'<pre>'; flush();
+$modx->setLogTarget('ECHO');
+echo 'Packing ' . PKG_NAME_LOWER . '-' . PKG_VERSION . '-' . PKG_RELEASE . '<pre>';
+flush();
 
-$modx->loadClass('transport.modPackageBuilder','',false, true);
+$modx->loadClass('transport.modPackageBuilder', '', false, true);
 $builder = new modPackageBuilder($modx);
-$builder->directory = dirname(dirname(__FILE__)).'/_packages/';
-$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
-$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
-$modx->getService('lexicon','modLexicon');
-
+$builder->directory = dirname(dirname(__FILE__)) . '/_packages/';
+$builder->createPackage(PKG_NAME_LOWER, PKG_VERSION, PKG_RELEASE);
+$builder->registerNamespace(PKG_NAME_LOWER, false, true, '{core_path}components/' . PKG_NAME_LOWER . '/', '{assets_path}components/' . PKG_NAME_LOWER . '/');
+$modx->getService('lexicon', 'modLexicon');
 
 
 /* create category */
-$category= $modx->newObject('modCategory');
-$category->set('id',1);
-$category->set('category',PKG_NAME);
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in category.'); flush();
+$category = $modx->newObject('modCategory');
+$category->set('id', 1);
+$category->set('category', PKG_NAME);
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in category.');
+flush();
 
 /* add plugins */
-$plugins = include $sources['data'].'transport.plugins.php';
-if (!is_array($plugins)) { $modx->log(modX::LOG_LEVEL_FATAL,'Adding plugins failed.'); }
-$attributes= array(
+$plugins = include $sources['data'] . 'transport.plugins.php';
+if (!is_array($plugins)) {
+    $modx->log(modX::LOG_LEVEL_FATAL, 'Adding plugins failed.');
+}
+$attributes = [
     xPDOTransport::UNIQUE_KEY => 'name',
     xPDOTransport::PRESERVE_KEYS => false,
     xPDOTransport::UPDATE_OBJECT => true,
     xPDOTransport::RELATED_OBJECTS => true,
-    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-        'PluginEvents' => array(
+    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => [
+        'PluginEvents' => [
             xPDOTransport::PRESERVE_KEYS => true,
             xPDOTransport::UPDATE_OBJECT => false,
-            xPDOTransport::UNIQUE_KEY => array('pluginid','event'),
-        ),
-    ),
-);
+            xPDOTransport::UNIQUE_KEY => ['pluginid', 'event'],
+        ],
+    ],
+];
 foreach ($plugins as $plugin) {
     $vehicle = $builder->createVehicle($plugin, $attributes);
     $builder->putVehicle($vehicle);
 }
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($plugins).' plugins.'); flush();
-unset($plugins,$plugin,$attributes);
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($plugins) . ' plugins.');
+flush();
+unset($plugins, $plugin, $attributes);
 
 /* add snippets */
-$snippets = include $sources['data'].'transport.snippets.php';
+$snippets = include $sources['data'] . 'transport.snippets.php';
 if (is_array($snippets)) {
-    $category->addMany($snippets,'Snippets');
-} else { $modx->log(modX::LOG_LEVEL_FATAL,'Adding snippets failed.'); }
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($snippets).' snippets.'); flush();
+    $category->addMany($snippets, 'Snippets');
+} else {
+    $modx->log(modX::LOG_LEVEL_FATAL, 'Adding snippets failed.');
+}
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($snippets) . ' snippets.');
+flush();
 unset($snippets);
 
 /* Settings */
-$settings = include_once $sources['data'].'transport.settings.php';
-$attributes= array(
+$settings = include_once $sources['data'] . 'transport.settings.php';
+$attributes = [
     xPDOTransport::UNIQUE_KEY => 'key',
     xPDOTransport::PRESERVE_KEYS => true,
     xPDOTransport::UPDATE_OBJECT => false,
-);
-if (!is_array($settings)) { $modx->log(modX::LOG_LEVEL_FATAL,'Adding settings failed.'); }
+];
+if (!is_array($settings)) {
+    $modx->log(modX::LOG_LEVEL_FATAL, 'Adding settings failed.');
+}
 foreach ($settings as $setting) {
-    $vehicle = $builder->createVehicle($setting,$attributes);
+    $vehicle = $builder->createVehicle($setting, $attributes);
     $builder->putVehicle($vehicle);
 }
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($settings).' system settings.'); flush();
-unset($settings,$setting,$attributes);
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($settings) . ' system settings.');
+flush();
+unset($settings, $setting, $attributes);
 
 /* create category vehicle */
-$attr = array(
+$attr = [
     xPDOTransport::UNIQUE_KEY => 'category',
     xPDOTransport::PRESERVE_KEYS => false,
     xPDOTransport::UPDATE_OBJECT => true,
     xPDOTransport::RELATED_OBJECTS => true,
-    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-        'Snippets' => array(
+    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => [
+        'Snippets' => [
             xPDOTransport::PRESERVE_KEYS => false,
             xPDOTransport::UPDATE_OBJECT => true,
             xPDOTransport::UNIQUE_KEY => 'name',
-        ),
-    )
-);
-$vehicle = $builder->createVehicle($category,$attr);
-$vehicle->resolve('file',array(
+        ],
+    ]
+];
+$vehicle = $builder->createVehicle($category, $attr);
+$vehicle->resolve('file', [
     'source' => $sources['source_core'],
     'target' => "return MODX_CORE_PATH . 'components/';",
-));
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in resolvers.'); flush();
+]);
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in resolvers.');
+flush();
 $builder->putVehicle($vehicle);
 
 /* now pack in the license file, readme and setup options */
-$builder->setPackageAttributes(array(
+$builder->setPackageAttributes([
     'license' => file_get_contents($sources['docs'] . 'license.txt'),
     'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
     'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
     /*'setup-options' => array(
         'source' => $sources['build'].'setup.options.php',
     ),*/
-));
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in package attributes.'); flush();
+]);
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in package attributes.');
+flush();
 
-$modx->log(modX::LOG_LEVEL_INFO,'Packing...'); flush();
+$modx->log(modX::LOG_LEVEL_INFO, 'Packing...');
+flush();
 $builder->pack();
 
 $mtime = microtime();
@@ -153,6 +168,4 @@ $tend = $mtime;
 $totalTime = ($tend - $tstart);
 $totalTime = sprintf("%2.4f s", $totalTime);
 
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
-
-?>
+$modx->log(modX::LOG_LEVEL_INFO, "\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
